95fb53df50860c37fabbe99ea8aff819227e748e
AlanCFleming <alanfleming1998@gmail.com>
2020-07-04 12:35:28 -0400

    Added checks for input being a string in the correct format

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ diceParser.parse("5d6 +2")
 diceParser.parse("15d4 KH5")
 diceParser.parse("d4 +2")
 diceParser.parse("6d6 dl2")
-
+diceParser.parse("3d +2")
+diceParser.parse("hello")
+diceParser.parse("d6 +2 +2")
--- scripts/diceParser.py
+++ scripts/diceParser.py
@@ -3,6 +3,17 @@
 import re
 
 def parse( diceString ):
+
+    #check if diceString is a string
+    if not isinstance(diceString, str):
+        print("The provided input must be a string")
+        return
+
+    #check if valid string
+    if not re.match('[0-9]*[dD][0-9]+\s*([+-][0-9]*)?\s*([kKdD][lLhH][0-9]*)?', diceString):
+        print("The privided string is not a valid dice role")
+        return
+
     #get the type of dice
     sides = re.search('[dD][0-9]+', diceString)
     if sides:
